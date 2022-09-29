2cff894e137c0bf8f4c9c4cd4bd158f36a2fa006
apalazzolo <antoine.palazzolo@insee.fr>
2022-09-29 09:11:54 +0000

    Minor corrections

--- R/dummyModels.R
+++ R/dummyModels.R
@@ -73,24 +73,24 @@ tourism_preds_naive_1m
 
 
 #########################################
-# 2. Models re-using the 6 last relevant values
+# 2. Models re-using the x last relevant values
 #########################################
 
 # 2.a) PPI
-# Use the value of the last 6 available months
+# Use the value of the last 3 available months
 
-ppi_preds_naive_6m <- data.frame(matrix(ncol = length(countries_PPI), nrow = 1))
-colnames(ppi_preds_naive_6m) <- countries_PPI
+ppi_preds_naive_3m <- data.frame(matrix(ncol = length(countries_PPI), nrow = 1))
+colnames(ppi_preds_naive_3m) <- countries_PPI
 
 for (country in countries_PPI){
   last_available_values <- tail(ppi_data %>%
                                  select(country) %>%
                                  drop_na(),
-                               n = 6)
-  ppi_preds_naive_6m[[country]][1] <- mean(as.matrix(last_available_values))
+                               n = 3)
+  ppi_preds_naive_3m[[country]][1] <- mean(as.matrix(last_available_values))
 }
 
-ppi_preds_naive_6m
+ppi_preds_naive_3m
 
 # 2.b) PVI
 # Use the value of the last 6 available months
