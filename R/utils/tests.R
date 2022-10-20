7e147b2bddd51da4f1fe61fbf048aa7da6a0a6d0
apalazzolo <antoine.palazzolo@insee.fr>
2022-10-20 13:10:16 +0000

    add new data sources

--- R/utils/tests.R
+++ R/utils/tests.R
@@ -16,6 +16,8 @@ library(quantmod)
 brent_id <- "BZ=F"
 eur_usd_id <- "EURUSD=X"
 sp500_id <- "^GSPC"
+cac40_id <- "^FCHI"
+eurostoxx500_id <- "^STOXX50E"
 
 #########################################
 # 2) Import variables
@@ -30,5 +32,9 @@ eur_usd_data <- data.frame(date=index(`EURUSD=X`), coredata(`EURUSD=X`))
 getSymbols(sp500_id, src="yahoo")
 sp500_data <- data.frame(date=index(`GSPC`), coredata(`GSPC`))
 
+getSymbols(cac40_id, src="yahoo")
+cac40_data <- data.frame(date=index(`FCHI`), coredata(`FCHI`))
 
+getSymbols(eurostoxx500_id, src="yahoo")
+eurostoxx500_data <- data.frame(date=index(`STOXX50E`), coredata(`STOXX50E`))
 
