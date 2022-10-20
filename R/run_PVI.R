d7cf6ee202a511702d17b88479513fded9c9c4a6
tfaria <thomas.faria@insee.fr>
2022-10-20 15:32:28 +0000

    adjusting PVI routines

--- R/run_PVI.R
+++ R/run_PVI.R
@@ -6,6 +6,7 @@
 source("R/utils/globalVariables.R")
 source("R/utils/getData.R")
 source("R/utils/save_entries.R")
+source("R/utils/plot_routines.R")
 
 #### Import the data ####
 data <- getData("PVI")
@@ -14,6 +15,20 @@ data <- getData("PVI")
 source("R/PVI/LastPeriod_model.R") # to be converted to functions
 source("R/PVI/S-ARIMA.R") # to be converted to functions
 
+#### Plotting the results #### 
+predictions <- bind_rows(list(
+  "entry_1"= preds_naive_1m%>%mutate(Entries = "Naive"), 
+  "entry_2"= preds_sarima%>%mutate(Entries = "S-ARIMA"))
+)
+
+plot_preds(data$PVI, predictions, countries_PVI[1:9], ncol = 3)
+plot_preds(data$PVI, predictions, countries_PVI[10:18], ncol = 3)
+plot_preds(data$PVI, predictions, countries_PVI[-1:-18], ncol = 3)
+
+
 #### Save the results #### 
-entries <- list("entry_1"= preds_naive_1m, "entry_2"= preds_sarima)
+entries <- list(
+  "entry_1"= preds_naive_1m%>%pull(value, Country), 
+  "entry_2"= preds_sarima%>%pull(value, Country)
+)
 save_entries(entries, "Submissions/PVI/results_october.json")
