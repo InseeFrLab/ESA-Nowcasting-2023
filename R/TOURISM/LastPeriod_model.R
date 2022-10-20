ed61bbdb36de866db9b661f1e9377fc4bf6e83c9
tfaria <thomas.faria@insee.fr>
2022-10-20 15:32:45 +0000

    adjusting Tourism routines

--- R/TOURISM/LastPeriod_model.R
+++ R/TOURISM/LastPeriod_model.R
@@ -6,20 +6,25 @@
 # Required packages
 #########################################
 library(dplyr)
-
+library(lubridate)
 #############################################
 # Use the value of preceding year 
 #############################################
 
-preds_naive_1y <- c()
+preds_naive_1y <- tibble(Country=character(),
+                                        Date=as.POSIXct(NA),
+                                        value=numeric()
+)
+
 for (country in countries_tourism){
+  pred <- data$TOURISM%>%
+                filter(geo %in% country)%>%
+                arrange(time)%>%
+                tail(12)%>% # take the last 12 months
+                head(1)%>% # select the value of reference 1-year before
+                pull(values, time)
   
-  preds_naive_1y <- c(preds_naive_1y, data$TOURISM%>%
-                                filter(geo %in% country)%>%
-                                arrange(time)%>%
-                                tail(12)%>% # take the last 12 months
-                                head(1)%>% # select the value of reference 1-year before
-                                pull(values))
-  
+  preds_naive_1y <- preds_naive_1y%>%
+    add_row(Country=country, Date=as.POSIXct(names(pred)) %m+% months(1), value=as.numeric(pred))
+
 }
-preds_naive_1y <- setNames(preds_naive_1y, nm = countries_tourism)
--- R/run_TOURISM.R
+++ R/run_TOURISM.R
@@ -6,6 +6,7 @@
 source("R/utils/globalVariables.R")
 source("R/utils/getData.R")
 source("R/utils/save_entries.R")
+source("R/utils/plot_routines.R")
 
 #### Import the data ####
 data <- getData("TOURISM")
@@ -14,6 +15,20 @@ data <- getData("TOURISM")
 source("R/TOURISM/LastPeriod_model.R") # to be converted to functions
 source("R/TOURISM/S-ARIMA.R") # to be converted to functions
 
+#### Plotting the results #### 
+predictions <- bind_rows(list(
+  "entry_1"= preds_naive_1y%>%mutate(Entries = "Naive"), 
+  "entry_2"= preds_sarima%>%mutate(Entries = "S-ARIMA"))
+)
+
+plot_preds(data$TOURISM, predictions, countries_tourism[1:9], ncol = 3)
+plot_preds(data$TOURISM, predictions, countries_tourism[10:18], ncol = 3)
+plot_preds(data$TOURISM, predictions, countries_tourism[-1:-18], ncol = 3)
+
+
 #### Save the results #### 
-entries <- list("entry_1"= preds_naive_1y, "entry_2"= preds_sarima)
+entries <- list(
+  "entry_1"= preds_naive_1y%>%pull(value, Country), 
+  "entry_2"= preds_sarima%>%pull(value, Country)
+)
 save_entries(entries, "Submissions/TOURISM/results_october.json")
