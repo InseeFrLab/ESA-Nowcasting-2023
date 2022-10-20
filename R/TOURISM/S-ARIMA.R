4d754654accab6ada90047f77b9771758874f978
tfaria <thomas.faria@insee.fr>
2022-10-20 09:01:14 +0000

    adding 2 files that produces forecasts for TOURISM

---
+++ R/TOURISM/S-ARIMA.R
@@ -0,0 +1,26 @@
+###############################################################################
+#                  Time series models : (S)ARIMA                              #  
+###############################################################################
+
+#########################################
+# Required packages
+#########################################
+library(dplyr)
+library(astsa)
+
+#########################################
+# Estimate a SARIMA
+#########################################
+
+preds_sarima <- c()
+for (country in countries_tourism){
+  
+  pred <- sarima.for(data$TOURISM%>%filter(geo %in% country & time <= early_date_to_stop)%>%pull(values),
+                     1,
+                     0, 1, 1, 
+                     0, 1, 1, 12,
+                     plot = FALSE)$pred[1]
+  
+  preds_sarima <- c(preds_sarima, pred)
+}
+preds_sarima <- setNames(preds_sarima, nm = countries_tourism)
