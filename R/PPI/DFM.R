85642171476ad4213cc11901aff3bb8c0a95ad8b
tfaria <thomas.faria@insee.fr>
2022-10-27 16:02:33 +0000

    using a loop so that we estimate one model per country

--- R/PPI/DFM.R
+++ R/PPI/DFM.R
@@ -8,94 +8,130 @@
 library(dplyr)
 library(tidyverse)
 library(dfms)
-library(tsibble)
 library(xts)
-data <- getData("PPI")
-install.packages(
-  "tsibble"
+library(lubridate)
+
+#data <- getData("PPI")
+
+library("progress")
+
+date_to_pred <- "2022-10-01"
+
+defaultW <- getOption("warn") 
+options(warn = -1) 
+#########################################
+# Loop
+#########################################
+preds_dfm <- tibble(Country=character(),
+                    Date=as.POSIXct(NA),
+                    value=numeric()
 )
 
-#########################################
-# Estimate a DFS
-#########################################
-
-ppi <- data$PPI%>%
-  filter(geo %in% c("FR"))%>%
-  mutate(var = "PPI")%>%
-  pivot_wider(names_from =  c(geo, var, nace_r2), values_from = values)
-
-ppi_nace2 <- data$PPI_NACE2%>%
-  filter(geo %in% c("FR"))%>%
-  mutate(var = "PPI")%>%
-  pivot_wider(names_from =  c(geo, var, nace_r2), values_from = values)
-
-ipi <- data$IPI%>%
-  filter(geo %in% c("FR"))%>%
-  mutate(var = "IPI")%>%
-  pivot_wider(names_from =  c(geo, var, nace_r2), values_from = values)
-
-psurvey <- data$PSURVEY%>%
-  filter(geo %in% c("FR"))%>%
-  mutate(var = "PSURVEY")%>%
-  pivot_wider(names_from =  c(geo, var, indic), values_from = values)
-
-DB <- list(ppi, ppi_nace2, ipi, psurvey)%>%
-  reduce(full_join, by="time")%>%
-  arrange(time)
-
-DB <- xts(as.data.frame(DB[,-1]), order.by=as.Date(DB[,1]%>%pull()))
-plot(scale(DB), lwd = 1)
-
-DB_diff = diff(DB)
-plot(scale(DB_diff), lwd = 1)
-
-ic = ICr(DB_diff)
-
-plot(ic)
-screeplot(ic)
-
-r <- as.double(names(sort(table(ic$r.star),decreasing=TRUE)[1]))
-lag <- as.double(names(sort(table(vars::VARselect(ic$F_pca[, 1:5])$selection),decreasing=TRUE)[1]))
-
-## If we want to remove seasonnality it's here
-
-model1 <- DFM(DB_diff, r = r, p = lag)
-print(model1)
-plot(model1)
-
-dfm_summary <- summary(model1)
-print(dfm_summary)
-
-plot(resid(model1, orig.format = TRUE)$`FR_PPI_B-E36`)
-plot(fitted(model1, orig.format = TRUE)$`FR_PPI_B-E36`)
-
-
-xx <- scale(DB)
-attr(xx,"scaled:center")
-plot(DB$`FR_PPI_B-E36`)
-plot(resid(model1, orig.format = TRUE)$`FR_PPI_B-E36`)
-plot(fitted(model1, orig.format = TRUE)$`FR_PPI_B-E36`)
-DB_diff$`FR_PPI_B-E36` - fitted(model1, orig.format = TRUE)$`FR_PPI_B-E36`
-
-# 12-period ahead DFM forecast
-if (is.na(DB["2022-09-01"]$`FR_PPI_B-E36`)) {
-  h = 2
-}else{
-  h = 1
+pb <- progress_bar$new(
+  format = " [:bar] :percent eta: :eta \n",
+  total = 26, clear = FALSE, width= 60)
+for (country in c("DE", "FR", "IT","ES", "NL", "BE", "AT","FI", "PT", "EL")) {
+  cat(paste0("Running estimation for ", country, "\n"))
+  
+  pb$tick()
+    
+  #########################################
+  # Reshaping the data
+  #########################################  
+  ppi <- data$PPI%>%
+    mutate(var = "PPI")%>%
+    filter(geo %in% country)%>%
+    pivot_wider(names_from =  c(geo, var, nace_r2), values_from = values)
+  
+  ppi_nace2 <- data$PPI_NACE2%>%
+    mutate(var = "PPI")%>%
+    filter(geo %in% country)%>%
+    pivot_wider(names_from =  c(geo, var, nace_r2), values_from = values)
+  
+  ipi <- data$IPI%>%
+    mutate(var = "IPI")%>%
+    filter(geo %in% country)%>%
+    pivot_wider(names_from =  c(geo, var, nace_r2), values_from = values)
+  
+  psurvey <- data$PSURVEY%>%
+    mutate(var = "PSURVEY")%>%
+    filter(geo %in% country)%>%
+    pivot_wider(names_from =  c(geo, var, indic), values_from = values)
+  
+  DB <- list(ppi, ppi_nace2, ipi, psurvey)%>%
+    reduce(full_join, by="time")%>%
+    filter(time>as.Date("2000-01-01"))%>% # Max 2004-09 # 2003 ok BG
+    arrange(time)
+  
+  if (last(DB$time) %--% ymd(date_to_pred) %/% months(1) > 1){
+    line_to_add <- last(DB$time) %--% ymd(date_to_pred) %/% months(1) - 1
+    for (i in 1:line_to_add) {
+      cat("No data in", as.character(last(DB$time) %m+% months(1)),"for country ", country, "\n")
+      DB <- DB %>%
+        add_row(time = last(DB$time) %m+% months(1))
+    }
+  }
+  
+  DB <- xts(as.data.frame(DB[,-1]), order.by=as.Date(DB[,1]%>%pull()))
+#  plot(scale(DB), lwd = 1)
+  
+  # We differenciate the series
+  DB_diff = diff(DB)
+#  plot(scale(DB_diff), lwd = 1)
+  
+  #########################################
+  # Determination of parameters
+  #########################################
+  
+  # We determine the optimal number of factor and lags
+  ic = ICr(DB_diff)
+#  plot(ic)
+#  screeplot(ic)
+  r <- as.double(names(sort(table(ic$r.star),decreasing=TRUE)[1]))
+  lag <- as.double(names(sort(table(vars::VARselect(ic$F_pca[, 1:r])$selection),decreasing=TRUE)[1]))
+  
+  #########################################
+  # Seasonality removal (TO BE ADD HERE)
+  #########################################
+  
+  #########################################
+  # Simulation of DFM
+  #########################################
+  
+  model <- tryCatch({
+    DFM(DB_diff, r = r, p = lag)
+    
+  }
+  ,
+  error=function(e) {
+    cat(paste0("Failed for country ", country, "\n"))
+    e
+  }
+  )
+  
+  if(inherits(model, "error")) next
+  
+  #########################################
+  # Forecasting
+  #########################################
+  current_month <- ymd(date_to_pred) %m-% months(1)
+  if (is.na(DB[current_month][,paste0(country, "_PPI_B-E36")])) {
+    h = 2
+  }else{
+    h = 1
+  }
+  
+  fc = predict(model, h = h)
+  
+  #########################################
+  # Storing the predictions
+  #########################################
+  
+  last_available_date<- ymd(date_to_pred) %m-% months(h)
+  pred <- as.double(DB[,paste0(country, "_PPI_B-E36")][last_available_date] + 
+            sum(fc$X_fcst[, paste0(country, "_PPI_B-E36")]))
+  
+  preds_dfm <- preds_dfm %>%
+    add_row(Country=country, Date=as.POSIXct(date_to_pred), value=round(pred,1))
+  
 }
-
-fc = predict(model1, h = h)
-
-sum(fc$X_fcst[,"FR_PPI_B-E36"])
-DB_diff[,"FR_PPI_B-E36"]
-
-DB[,"FR_PPI_B-E36"]["2022-09-01"] - DB[,"FR_PPI_B-E36"]["2022-08-01"]
-
-# use lubridate to switch among dates 
-
-DB["2020-08-01/2022-08-01","FR_PPI_B-E36"] - lag(DB["2020-08-01/2022-08-01","FR_PPI_B-E36"])
-diff(DB["2020-08-01/2022-08-01","FR_PPI_B-E36"],lag=1,differences=1)
-
-pred <- as.double(DB[,"FR_PPI_B-E36"]["2022-08-01"] + sum(fc$X_fcst[,"FR_PPI_B-E36"]))
-
-
