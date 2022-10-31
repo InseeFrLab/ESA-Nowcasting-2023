347e51e112174afc53cf8dd912a314425d7ee3c7
aqlt <alain.quartier@yahoo.fr>
2022-10-31 18:44:32 +0000

    add_entries to add entries to existing json file

--- R/utils/save_entries.R
+++ R/utils/save_entries.R
@@ -5,3 +5,10 @@ save_entries <- function(entries, filename){
   file = rjson::toJSON(entries)
   write(jsonlite::prettify(file), filename)
 }
+
+add_entries <- function(entries, filename){
+  current_file = rjson::fromJSON(paste0(readLines(filename), collapse = ""))
+  c(current_file, entries)
+  file = rjson::toJSON(c(current_file, entries))
+  write(jsonlite::prettify(file), filename)
+}
