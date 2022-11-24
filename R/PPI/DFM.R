b35e506268f5b2a274a001c3e1f2fd03d0817f50
tfaria <thomas.faria@insee.fr>
2022-11-24 14:29:02 +0000

    removing also lags when it doesn't converge

--- R/PPI/DFM.R
+++ R/PPI/DFM.R
@@ -238,7 +238,13 @@ for (country in countries_PPI) {
       converged <- model$converged
     }
     # We reduce the number of factor, so that we can resimulate when it has failed
-    r <- r - 1
+    if (r > 1) {
+      r <- r - 1 
+      lag <- as.double(names(sort(table(vars::VARselect(ic$F_pca[, 1:r])$selection), decreasing = TRUE)[1]))
+      if (lag > max_lags) lag <- max_lags
+    }else{
+      lag <- lag - 1
+    }
   }
 
   #########################################
