b88060b66f418331c428bda724db4db2305db512
apalazzolo <antoine.palazzolo@insee.fr>
2022-12-14 11:56:17 +0000

    Replace naive by XGBoost_diff & submissions december

--- R/run_PPI.R
+++ R/run_PPI.R
@@ -20,16 +20,16 @@ month <- "december"
 source("R/PPI/LastPeriod_model.R") # to be converted to functions
 source("R/PPI/Regarima.R") # to be converted to functions
 source("R/PPI/XGBoost.R") # to be converted to functions
-# source("R/PPI/XGBoost_diff.R") # to be converted to functions
+source("R/PPI/XGBoost_diff.R") # to be converted to functions
 source("R/PPI/DFM.R") # to be converted to functions
 source("R/PPI/ETS.R") # to be converted to functions
 
 #### Plotting the results ####
 predictions <- bind_rows(list(
-  "entry_1" = preds_naive_1m %>% mutate(Entries = "Naive"),
+  # "entry_1" = preds_naive_1m %>% mutate(Entries = "Naive"),
   "entry_2" = preds_regarima %>% mutate(Entries = "REG-ARIMA"),
   "entry_3" = preds_xgboost %>% mutate(Entries = "XGBoost"),
-  # "entry_3" = preds_xgboost_diff %>% mutate(Entries = "XGBoost_diff"),
+  "entry_1" = preds_xgboost_diff %>% mutate(Entries = "XGBoost_diff"), # A garder ?
   "entry_4" = preds_dfm %>% mutate(Entries = "DFM"),
   "entry_5" = preds_ets %>% mutate(Entries = "ETS")
 ))
@@ -40,10 +40,10 @@ plot_preds(data$PPI, predictions, countries_PPI[-1:-18], ncol = 3)
 
 #### Analyse the residuals
 resids <- bind_rows(list(
-  "entry_1" = resid_naive_1m %>% mutate(Entries = "Naive"),
+  # "entry_1" = resid_naive_1m %>% mutate(Entries = "Naive"),
   "entry_2" = resid_regarima %>% mutate(Entries = "REG-ARIMA"),
   "entry_3" = resid_xgboost %>% mutate(Entries = "XGBoost"),
-  # "entry_3" = resid_xgboost_diff %>% mutate(Entries = "XGBoost_diff"),
+  "entry_1" = resid_xgboost_diff %>% mutate(Entries = "XGBoost_diff"), # A garder ?
   "entry_4" = resid_dfm %>% mutate(Entries = "DFM"),
   "entry_5" = resid_ets %>% mutate(Entries = "ETS")
 ))
@@ -53,10 +53,10 @@ plot_statistics(get_metrics(resids, countries_PPI, current_date, as.Date("2010-0
 
 #### Save the results ####
 entries <- list(
-  "entry_1" = lapply(split(preds_naive_1m %>% pull(value, Country), names(preds_naive_1m %>% pull(value, Country))), unname),
+  # "entry_1" = lapply(split(preds_naive_1m %>% pull(value, Country), names(preds_naive_1m %>% pull(value, Country))), unname),
   "entry_2" = lapply(split(preds_regarima %>% pull(value, Country), names(preds_regarima %>% pull(value, Country))), unname),
   "entry_3" = lapply(split(preds_xgboost %>% pull(value, Country), names(preds_xgboost %>% pull(value, Country))), unname),
-  # "entry_3" = lapply(split(preds_xgboost_diff %>% pull(value, Country), names(preds_xgboost_diff %>% pull(value, Country))), unname),
+  "entry_1" = lapply(split(preds_xgboost_diff %>% pull(value, Country), names(preds_xgboost_diff %>% pull(value, Country))), unname), # A garder ?
   "entry_4" = lapply(split(preds_dfm %>% pull(value, Country), names(preds_dfm %>% pull(value, Country))), unname),
   "entry_5" = lapply(split(preds_ets %>% pull(value, Country), names(preds_ets %>% pull(value, Country))), unname)
 )
@@ -79,5 +79,3 @@ system(
     paste0("s3/projet-esa-nowcasting/data/PPI/data_", month, ".RData")
   )
 )
-# entries = list("entry_5"= preds_ets%>%pull(value, Country))
-# add_entries(entries, "Submissions/PPI/results_november.json")
