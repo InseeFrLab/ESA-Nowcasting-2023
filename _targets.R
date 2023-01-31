05680f224b0bf91431f33c9d382997f7a737a0d9
tfaria <thomas.faria@insee.fr>
2023-01-31 10:02:02 +0000

    add plots to the pipeline

--- _targets.R
+++ _targets.R
@@ -3,7 +3,7 @@ library(targets)
 
 # Set target options:
 tar_option_set(
-  packages = c("xts", "lubridate", "dplyr", "dfms", "cowplot", "jsonlite", "data.table"),
+  packages = c("xts", "lubridate", "dplyr", "dfms", "cowplot", "jsonlite", "data.table", "ggplot2"),
   memory = "transient",
   garbage_collection = TRUE
 )
@@ -68,5 +68,162 @@ list(
   tar_target(
     name = dfms_tourism,
     command =  run_DFMs("TOURISM", challenges, data, models)
+  ),
+  tar_target(
+    name = predictions_ppi,
+    command =  bind_rows(list(
+      "entry_1" = regarima_ppi$preds %>% mutate(Entries = "REG-ARIMA"),
+      "entry_2" = dfms_ppi$preds %>% mutate(Entries = "DFM"),
+      "entry_3" = ets_ppi$preds %>% mutate(Entries = "ETS")
+    ))
+  ),
+  tar_target(
+    name = predictions_pvi,
+    command =  bind_rows(list(
+      "entry_1" = regarima_pvi$preds %>% mutate(Entries = "REG-ARIMA"),
+      "entry_2" = dfms_pvi$preds %>% mutate(Entries = "DFM"),
+      "entry_3" = ets_pvi$preds %>% mutate(Entries = "ETS")
+    ))
+  ),
+  tar_target(
+    name = predictions_tourism,
+    command =  bind_rows(list(
+      "entry_1" = regarima_tourism$preds %>% mutate(Entries = "REG-ARIMA"),
+      "entry_2" = dfms_tourism$preds %>% mutate(Entries = "DFM"),
+      "entry_3" = ets_tourism$preds %>% mutate(Entries = "ETS")
+    ))
+  ),
+  tar_target(
+    name = resids_ppi,
+    command =  bind_rows(list(
+      "entry_1" = regarima_ppi$resids %>% mutate(Entries = "REG-ARIMA"),
+      "entry_2" = dfms_ppi$resids %>% mutate(Entries = "DFM"),
+      "entry_3" = ets_ppi$resids %>% mutate(Entries = "ETS")
+    ))
+  ),
+  tar_target(
+    name = resids_pvi,
+    command =  bind_rows(list(
+      "entry_1" = regarima_pvi$resids %>% mutate(Entries = "REG-ARIMA"),
+      "entry_2" = dfms_pvi$resids %>% mutate(Entries = "DFM"),
+      "entry_3" = ets_pvi$resids %>% mutate(Entries = "ETS")
+    ))
+  ),
+  tar_target(
+    name = resids_tourism,
+    command =  bind_rows(list(
+      "entry_1" = regarima_tourism$resids %>% mutate(Entries = "REG-ARIMA"),
+      "entry_2" = dfms_tourism$resids %>% mutate(Entries = "DFM"),
+      "entry_3" = ets_tourism$resids %>% mutate(Entries = "ETS")
+    ))
+  ),
+  tar_target(
+    name = plot_preds_ppi_1,
+    command =  plot_preds("PPI", challenges, data, predictions_ppi, challenges$PPI$countries[1:4])
+  ),
+  tar_target(
+    name = plot_preds_ppi_2,
+    command =  plot_preds("PPI", challenges, data, predictions_ppi, challenges$PPI$countries[5:8])
+  ),
+  tar_target(
+    name = plot_preds_ppi_3,
+    command =  plot_preds("PPI", challenges, data, predictions_ppi, challenges$PPI$countries[9:12])
+  ),
+  tar_target(
+    name = plot_preds_ppi_4,
+    command =  plot_preds("PPI", challenges, data, predictions_ppi, challenges$PPI$countries[13:16])
+  ),
+  tar_target(
+    name = plot_preds_ppi_5,
+    command =  plot_preds("PPI", challenges, data, predictions_ppi, challenges$PPI$countries[17:20])
+  ),
+  tar_target(
+    name = plot_preds_ppi_6,
+    command =  plot_preds("PPI", challenges, data, predictions_ppi, challenges$PPI$countries[21:24])
+  ),
+  tar_target(
+    name = plot_preds_ppi_7,
+    command =  plot_preds("PPI", challenges, data, predictions_ppi, challenges$PPI$countries[25:26])
+  ),
+  tar_target(
+    name = plot_preds_pvi_1,
+    command =  plot_preds("PVI", challenges, data, predictions_pvi, challenges$PVI$countries[1:4])
+  ),
+  tar_target(
+    name = plot_preds_pvi_2,
+    command =  plot_preds("PVI", challenges, data, predictions_pvi, challenges$PVI$countries[5:8])
+  ),
+  tar_target(
+    name = plot_preds_pvi_3,
+    command =  plot_preds("PVI", challenges, data, predictions_pvi, challenges$PVI$countries[9:12])
+  ),
+  tar_target(
+    name = plot_preds_pvi_4,
+    command =  plot_preds("PVI", challenges, data, predictions_pvi, challenges$PVI$countries[13:16])
+  ),
+  tar_target(
+    name = plot_preds_pvi_5,
+    command =  plot_preds("PVI", challenges, data, predictions_pvi, challenges$PVI$countries[17:20])
+  ),
+  tar_target(
+    name = plot_preds_pvi_6,
+    command =  plot_preds("PVI", challenges, data, predictions_pvi, challenges$PVI$countries[21:23])
+  ),
+  tar_target(
+    name = plot_preds_tourism_1,
+    command =  plot_preds("TOURISM", challenges, data, predictions_tourism, challenges$TOURISM$countries[1:4])
+  ),
+  tar_target(
+    name = plot_preds_tourism_2,
+    command =  plot_preds("TOURISM", challenges, data, predictions_tourism, challenges$TOURISM$countries[5:8])
+  ),
+  tar_target(
+    name = plot_preds_tourism_3,
+    command =  plot_preds("TOURISM", challenges, data, predictions_tourism, challenges$TOURISM$countries[9:12])
+  ),
+  tar_target(
+    name = plot_preds_tourism_4,
+    command =  plot_preds("TOURISM", challenges, data, predictions_tourism, challenges$TOURISM$countries[13:16])
+  ),
+  tar_target(
+    name = plot_preds_tourism_5,
+    command =  plot_preds("TOURISM", challenges, data, predictions_tourism, challenges$TOURISM$countries[17:20])
+  ),
+  tar_target(
+    name = plot_preds_tourism_6,
+    command =  plot_preds("TOURISM", challenges, data, predictions_tourism, challenges$TOURISM$countries[21:24])
+  ),
+  tar_target(
+    name = plot_resids_ppi,
+    command = plot_statistics(get_metrics(resids_ppi, challenges$PPI$countries, challenges$DATES$current_date, as.Date("2022-01-01")))
+  ),
+  tar_target(
+    name = plot_resids_pvi,
+    command = plot_statistics(get_metrics(resids_pvi, challenges$PVI$countries, challenges$DATES$current_date, as.Date("2022-01-01")))
+  ),
+  tar_target(
+    name = plot_resids_tourism,
+    command = plot_statistics(get_metrics(resids_tourism, challenges$TOURISM$countries, as.Date("2019-12-31"), as.Date("2010-01-01")))
   )
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
