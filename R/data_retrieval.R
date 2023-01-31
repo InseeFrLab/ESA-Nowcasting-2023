2c97aa61b0261885cb29114266381ffa87e4cc8f
tfaria <thomas.faria@insee.fr>
2023-01-31 20:20:41 +0000

    recode a data retrieval fonction so that we use another api that works

--- R/data_retrieval.R
+++ R/data_retrieval.R
@@ -2,10 +2,8 @@ get_data_from_eurostat <- function(data_info) {
   subset_lists <- Filter(function(x) x$source == "Eurostat", data_info)
 
   data <- lapply(subset_lists, function(x) {
-    eurostat::get_eurostat(x$id,
-      select_time = "M",
-      filters = x$filters,
-      time_format = "date"
+    get_eurostat_json_thomas(x$id,
+      filters = x$filters
     ) |>
       dplyr::select(geo, names(x$filters)[3], time, values) |>
       tidyr::drop_na(values)
@@ -95,4 +93,116 @@ get_data <- function(data_info = yaml::read_yaml("data.yaml"),
   )
 
   return(Map(c, list_data, data_info))
+}
+
+get_eurostat_json_thomas <- function(id, filters = NULL,
+                                     type = c("code", "label", "both"),
+                                     lang = c("en", "fr", "de"),
+                                     stringsAsFactors = FALSE,
+                                     ...) {
+  
+  # get response
+  # url <- try(eurostat_json_url(id = id, filters = filters, lang = lang))
+  # if (class(url) == "try-error") { stop(paste("The requested data set cannot be found with the following specifications to get_eurostat_json function: ", "id: ", id, "/ filters: ", filters, "/ lang: ", lang))  }
+  url <- eurostat_json_url_thomas(id = id, filters = filters, lang = lang)
+  
+  # resp <- try(httr::GET(url, ...))
+  # if (class(resp) == "try-error") { stop(paste("The requested url cannot be found within the get_eurostat_json function:", url))  }
+  resp <- httr::RETRY("GET", url, terminate_on = c(404))
+  if (httr::http_error(resp)) {
+    stop(paste("The requested url cannot be found within the get_eurostat_json function:", url))
+  }
+  
+  status <- httr::status_code(resp)
+  
+  # check status and get json
+  
+  msg <- ". Some datasets are not accessible via the eurostat
+          interface. You can try to search the data manually from the comext
+  	  database at http://epp.eurostat.ec.europa.eu/newxtweb/ or bulk
+  	  download facility at
+  	  http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing
+  	  or annual Excel files
+  	  http://ec.europa.eu/eurostat/web/prodcom/data/excel-files-nace-rev.2"
+  
+  if (status == 200) {
+    jdat <- jsonlite::fromJSON(url)
+  } else if (status == 400) {
+    stop(
+      "Failure to get data. Probably invalid dataset id. Status code: ",
+      status, msg
+    )
+  } else if (status == 500) {
+    stop("Failure to get data. Probably filters did not return any data
+         or data exceeded query size limitation. Status code: ", status, msg)
+  } else if (status == 416) {
+    stop(
+      "Too many categories have been requested. Maximum is 50.",
+      status, msg
+    )
+  } else {
+    stop("Failure to get data. Status code: ", status, msg)
+  }
+  
+  # get json data
+  # dims <- jdat[[1]]$dimension # Was called like this in API v1.1
+  dims <- jdat$dimension # Switched to this with API v2.1
+  # ids <- dims$id # v1.1
+  ids <- jdat$id # v2.1
+  
+  dims_list <- lapply(dims[rev(ids)], function(x) {
+    y <- x$category$label
+    if (type[1] == "label") {
+      y <- unlist(y, use.names = FALSE)
+    } else if (type[1] == "code") {
+      y <- names(unlist(y))
+    } else if (type[1] == "both") {
+      y <- unlist(y)
+    } else {
+      stop("Invalid type ", type)
+    }
+  })
+  
+  variables <- expand.grid(dims_list,
+                           KEEP.OUT.ATTRS = FALSE,
+                           stringsAsFactors = stringsAsFactors
+  )
+  
+  # dat <- data.frame(variables[rev(names(variables))], values = jdat[[1]]$value) # v1.1
+  dat <- as.data.frame(variables[rev(names(variables))])
+  vals <- unlist(jdat$value, use.names = FALSE)
+  dat$values <- rep(NA, nrow(dat))
+  inds <- 1 + as.numeric(names(jdat$value)) # 0-indexed
+  if (!length(vals) == length(inds)) {
+    stop("Complex indexing not implemented.")
+  }
+  dat$values[inds] <- vals
+  
+  
+  
+  data <- tibble::as_tibble(dat) |>
+    dplyr::mutate(time = as.Date(paste(time, "01", sep = "-")))
+  return(data)
+}
+
+eurostat_json_url_thomas <- function(id, filters, lang) {
+  
+  # prepare filters for query
+  filters2 <- as.list(unlist(filters))
+  names(filters2) <- rep(names(filters), lapply(filters, length))
+  
+  # prepare url
+  url_list <- list(
+    scheme = "http",
+    hostname = "ec.europa.eu",
+    path = file.path(
+      "eurostat/api/dissemination/statistics/1.0/data",
+      id
+    ),
+    query = filters2
+  )
+  
+  class(url_list) <- "url"
+  url <- httr::build_url(url_list)
+  url
 }
