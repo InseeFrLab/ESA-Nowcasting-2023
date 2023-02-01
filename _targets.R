a8204721327be21bd17c046acc1025453eb07907
tfaria <thomas.faria@insee.fr>
2023-02-01 10:14:22 +0000

    making sure that each changes in a yaml is detected by targets + add a saving function target

--- _targets.R
+++ _targets.R
@@ -16,20 +16,42 @@ tar_source(files = "R")
 
 list(
   tar_target(
+    name = data_info_file,
+    command = "data.yaml",
+    format = "file"
+  ),
+  tar_target(
+    name = challenges_file,
+    command = "challenges.yaml",
+    format = "file"
+    
+  ),
+  tar_target(
+    name = models_file,
+    command = "models.yaml",
+    format = "file"
+  ),
+  tar_target(
     name = data_info,
-    command = yaml::read_yaml("data.yaml")
+    command = yaml::read_yaml(data_info_file),
   ),
   tar_target(
     name = challenges,
-    command = yaml::read_yaml("challenges.yaml")
+    command = yaml::read_yaml(challenges_file),
+
   ),
   tar_target(
     name = models,
-    command = yaml::read_yaml("models.yaml")
+    command = yaml::read_yaml(models_file),
   ),
   tar_target(
     name = data,
     command = get_data(data_info, challenges)
+  ),
+  tar_target(
+    name = data_saved,
+    command = save_data(data, challenges),
+    format = "file"
   ),
   tar_target(
     name = ets_ppi,
