466393417b2965579c16113a8bee44a780259231
aquartier <alain.quartier@yahoo.fr>
2023-03-15 10:46:01 +0000

    script init

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+git clone https://github.com/InseeFrLab/ESA-Nowcasting-2023.git
+
+cd ESA-Nowcasting-2023
+
 git config --global credential.helper store
 
 mc cp -r s3/projet-esa-nowcasting/targets-data/ .
