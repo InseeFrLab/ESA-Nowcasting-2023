fa50ea5397e0484a1287a0259e7a470c547b471b
pleblanc <pierre.leblanc@insee.fr>
2023-04-12 11:25:30 +0000

    Ajustements data.yaml

--- run_tourism.R
+++ run_tourism.R
@@ -65,22 +65,23 @@ list(
       challenge = "TOURISM"
     )
   ),
-  # tar_target(
-  #   name = lstm_tourism,
-  #   command = run_lstm_per_country(
-  #     data = data,
-  #     config_models = models,
-  #     config_env = challenges,
-  #     challenge = "TOURISM"
-  #   )
-  # ),
+  tar_target(
+    name = lstm_tourism,
+    command = run_lstm_per_country(
+      data = data,
+      config_models = models,
+      config_env = challenges,
+      challenge = "TOURISM"
+    )
+  ),
   tar_target(
     name = predictions_tourism,
     command = bind_rows(list(
       "entry_1" = regarima_tourism$preds %>% mutate(Entries = "REG-ARIMA"),
       "entry_2" = dfms_tourism$preds %>% mutate(Entries = "DFM"),
       "entry_3" = ets_tourism$preds %>% mutate(Entries = "ETS"),
-      "entry_4" = xgboost_tourism$preds %>% mutate(Entries = "XGBOOST")
+      "entry_4" = xgboost_tourism$preds %>% mutate(Entries = "XGBOOST"),
+      "entry_5" = lstm_tourism$preds %>% mutate(Entries = "LSTM")
     ))
   ),
   tar_target(
@@ -89,7 +90,8 @@ list(
       "entry_1" = regarima_tourism$resids %>% mutate(Entries = "REG-ARIMA"),
       "entry_2" = dfms_tourism$resids %>% mutate(Entries = "DFM"),
       "entry_3" = ets_tourism$resids %>% mutate(Entries = "ETS"),
-      "entry_4" = xgboost_tourism$resids %>% mutate(Entries = "XGBOOST")
+      "entry_4" = xgboost_tourism$resids %>% mutate(Entries = "XGBOOST"),
+      "entry_5" = lstm_tourism$resids %>% mutate(Entries = "LSTM")
     ))
   ),
   tar_target(
@@ -127,7 +129,8 @@ list(
         "entry_1" = regarima_tourism,
         "entry_2" = dfms_tourism,
         "entry_3" = ets_tourism,
-        "entry_4" = xgboost_tourism
+        "entry_4" = xgboost_tourism,
+        "entry_5" = lstm_tourism
       ),
       challenges
     )
