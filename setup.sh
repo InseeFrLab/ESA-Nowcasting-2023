9b9889d106297c37d9787a7f66d59feaa4503d75
ThomasFaria <thomas.faria@ensae.fr>
2023-05-15 15:33:32 +0000

    chow

--- setup.sh
+++ setup.sh
@@ -2,10 +2,9 @@
 
 PROJECT_DIR=~/work/ESA-Nowcasting-2023
 git clone https://github.com/InseeFrLab/ESA-Nowcasting-2023.git $PROJECT_DIR
+chown -R onyxia:users $PROJECT_DIR/
 cd $PROJECT_DIR
 
 git config --global credential.helper store
 
 mc cp -r s3/projet-esa-nowcasting/targets-data/ .
-
-chown -R onyxia:users $PROJECT_DIR/
